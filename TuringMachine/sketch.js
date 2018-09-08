2480eed823683c37cf49186d62ae71a622e4abbe
Damián Meza Madrid <asdf1234damian@gmail.com>
2018-09-08 12:25:30 -0500

    Homework 1 ver 0.2

--- TuringMachine/sketch.js
+++ TuringMachine/sketch.js
@@ -1,29 +1,5 @@
-var tape;
+var cssSelector = anime({
+  targets: '.el',
+  translateX: 100
+});
 
-function setup() {
-  var canvasw = windowWidth;
-  var canvash = windowHeight;
-  createCanvas(canvasw/3,canvash/2)
-  tape = new inputTape();
-}
-
-function draw() {
-  background(100);
-
-  tape.display();
-}
-
-function inputTape() {
-  this.size=7;
-  this.display = function(){
-    for (var i = 0; i < this.size; i++) {
-      if (i%2==0) {
-        fill('white')
-      } else {
-        fill('black')
-      }
-      rect(i*100/7,200,100,100);
-    }
-  }
-
-}
