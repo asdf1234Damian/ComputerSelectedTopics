5b2f632f7b663670e97c4f9fc8c4246312d83db7
asdf1234Damian <asdf1234damian@gmail.com>
2018-10-13 02:36:41 -0500

    Deleted some unused files

--- CellularAutomaton/Patterns/comp.sh
+++ CellularAutomaton/Patterns/comp.sh
@@ -3,15 +3,15 @@ echo "Cleaning Files"
 rm txtfiles/*.txt
 echo "Compiling With Still Life"
 g++ source/generator.cpp -o generator
-./generator 2 2 3 3 3 >> txtfiles/LifeS2.txt
-./generator 3 2 3 3 3 >> txtfiles/LifeS3.txt
-./generator 4 2 3 3 3 >> txtfiles/LifeS4.txt
-#./generator 5 2 3 3 3 >> LifeS5.txt
+#./generator 2 2 3 3 3 >> txtfiles/LifeS2.txt
+#./generator 3 2 3 3 3 >> txtfiles/LifeS3.txt
+#./generator 4 2 3 3 3 >> txtfiles/LifeS4.txt
+./generator 5 2 3 3 3 >> LifeS5.txt
 #./generator 6 2 3 3 3 >> LifeS6.txt
 echo "Compiling Without Still Life"
 g++ source/generatorNoStillLife.cpp -o generatorNoStillLife
-./generatorNoStillLife 2 2 3 3 3 >> txtfiles/LifeS2NoStillLife.txt
-./generatorNoStillLife 3 2 3 3 3 >> txtfiles/LifeS3NoStillLife.txt
-./generatorNoStillLife 4 2 3 3 3 >> txtfiles/LifeS4NoStillLife.txt
-#./generator 5 2 3 3 3 >> LifeS5.txt
+#./generatorNoStillLife 2 2 3 3 3 >> txtfiles/LifeS2NoStillLife.txt
+#./generatorNoStillLife 3 2 3 3 3 >> txtfiles/LifeS3NoStillLife.txt
+#./generatorNoStillLife 4 2 3 3 3 >> txtfiles/LifeS4NoStillLife.txt
+./generator 5 2 3 3 3 >> LifeS5.txt
 #./generator 6 2 3 3 3 >> LifeS6.txt
